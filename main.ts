37bec92345ff13faed6406eedb639cf43f74f49c
David Williams <david@davidthings.com>
2023-09-10 19:02:34 -0700

    Adapting code to new color sensor chip

--- main.ts
+++ main.ts
@@ -26,19 +26,19 @@ namespace elizatools {
     //% group="Ring"
     //% cv.shadow="colorNumberPicker"
     export function headDirect(cv: number) {
-        let e = pins.createBuffer(3 * 3)
+        let f = pins.createBuffer(3 * 3)
 
-        let rColor = (cv >> 16) & 0xFF;
-        let gColor = (cv >> 8) & 0xFF;
-        let bColor = (cv >> 0) & 0xFF;
+        let rColor2 = (cv >> 16) & 0xFF;
+        let gColor2 = (cv >> 8) & 0xFF;
+        let bColor2 = (cv >> 0) & 0xFF;
 
-        for (let j = 0; j < 25; j++) {
-            e[j * 3 + 0] = gColor;
-            e[j * 3 + 1] = rColor;
-            e[j * 3 + 2] = bColor;
+        for (let k = 0; k < 25; k++) {
+            f[k * 3 + 0] = gColor2;
+            f[k * 3 + 1] = rColor2;
+            f[k * 3 + 2] = bColor2;
         }
         ws2812b.setBufferMode(DigitalPin.P16, ws2812b.BUFFER_MODE_RGB);
-        ws2812b.sendBuffer(e, DigitalPin.P16);
+        ws2812b.sendBuffer(f, DigitalPin.P16);
     }
 
     //% block
@@ -76,15 +76,26 @@ namespace elizatools {
         return Math.idiv( d, 58 );
     }
 
+    let colorSensorConfigured: boolean = false;
+    let colorSensorAddress: number = 0x39;
+    let colorSensorIdRegister: number = 0x92;
+    let colorSensorId:number = 0x90;
+
     //% block
     //% group="ColorSensor"
     export function checkColorSensor(): boolean {
-        let id3 = i2cReadRegister8( 41, 178 )
+        let id3 = i2cReadRegister8(colorSensorAddress, colorSensorIdRegister );
         // basic.showNumber( id )
-        return ( id3 == 68 )
+        return (id3 == colorSensorId )
     }
 
-    let colorSensorConfigured : boolean = false;
+    //% block
+    //% group="ColorSensor"
+    export function colorSensorReadId(): number {
+        let id3 = i2cReadRegister8(colorSensorAddress, colorSensorIdRegister);
+        // basic.showNumber( id )
+        return id3;
+    }
 
     //% block
     //% group="ColorSensor"
@@ -94,42 +105,42 @@ namespace elizatools {
         let gSense = 0;
         colorSensorConfigure();
         if (colorSensorConfigured) {
-            rSense = i2cReadRegister16(0x29, 0xA0 | 0x16);
-            gSense = i2cReadRegister16(0x29, 0xA0 | 0x18);
-            bSense = i2cReadRegister16(0x29, 0xA0 | 0x1A);
+            rSense = i2cReadRegister16(colorSensorAddress, 0xA0 | 0x16);
+            gSense = i2cReadRegister16(colorSensorAddress, 0xA0 | 0x18);
+            bSense = i2cReadRegister16(colorSensorAddress, 0xA0 | 0x1A);
         }
-        let rColor2 = ( rSense >> 8 ) & 0xFF;
-        let gColor2 = ( gSense >> 8 ) & 0xFF;
-        let bColor2 = ( bSense >> 8 ) & 0xFF;
+        let rColor22 = ( rSense >> 8 ) & 0xFF;
+        let gColor22 = ( gSense >> 8 ) & 0xFF;
+        let bColor22 = ( bSense >> 8 ) & 0xFF;
 
-        rColor2 = Math.pow( rColor2, 2.5 );
-        gColor2 = Math.pow( gColor2, 2.5 );
-        bColor2 = Math.pow( bColor2, 2.5 );
+        rColor22 = Math.pow( rColor22, 2.5 );
+        gColor22 = Math.pow( gColor22, 2.5 );
+        bColor22 = Math.pow( bColor22, 2.5 );
 
-        let cMax = (rColor2 > gColor2) ? rColor2 : gColor2;
-        cMax = (bColor2 > cMax) ? bColor2 : cMax;
+        let cMax = (rColor22 > gColor22) ? rColor22 : gColor22;
+        cMax = (bColor22 > cMax) ? bColor22 : cMax;
 
-        rColor2 = 0x0F * rColor2 / cMax;
-        gColor2 = 0x0F * gColor2 / cMax;
-        bColor2 = 0x0F * bColor2 / cMax;
+        rColor22 = 0x0F * rColor22 / cMax;
+        gColor22 = 0x0F * gColor22 / cMax;
+        bColor22 = 0x0F * bColor22 / cMax;
 
         // basic.showNumber( rColor >> 4 );
 
-        return ( rColor2 << 16 ) | (gColor2 << 8 ) | bColor2;
+        return ( rColor22 << 16 ) | (gColor22 << 8 ) | bColor22;
     }
 
     function colorSensorConfigure() {
         if ( !colorSensorConfigured && checkColorSensor() ) {
             // turn it on
             // Control Reg:  PON AEN
-            i2cWriteRegister(0x29, (0x00 + 0x80), 3)
+            i2cWriteRegister(colorSensorAddress, (0x00 + 0x80), 3)
             basic.pause(100)
             // RGB TIMING:FF 2.4ms, C0 150ms, 16b
-            i2cWriteRegister(0x29, (0x01 + 0x80), 0xC0 )
+            i2cWriteRegister(colorSensorAddress, (0x01 + 0x80), 0xC0 )
             // Wait Time:FF 2.4ms
-            i2cWriteRegister(0x29, (0x03 + 0x80), 0xFF )
+            i2cWriteRegister(colorSensorAddress, (0x03 + 0x80), 0xFF )
             // Persistance: 0x00 - IRQ every time
-            i2cWriteRegister(0x29, (0x0C + 0x80), 0)
+            i2cWriteRegister(colorSensorAddress, (0x0C + 0x80), 0)
 //            basic.showString( "C");
             colorSensorConfigured = true;
         }
